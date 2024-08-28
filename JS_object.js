567bc937a6bd11ddb4612d573717038063d3a89e
Bilal Bin Shafiq <bilalbinshafiq195@gmail.com>
2024-08-28 23:27:46 +0500

    Update JS_object.js

--- JS_object.js
+++ JS_object.js
@@ -1,1 +1,52 @@
-lkahd laskjf lsakjfalks jfsalkjf
+// Objects
+// In real life, objects are things like: houses, cars, people, animals, or any other subjects.
+
+// Objects Properties
+// A real life car has properties like weight and color:
+
+// Object Methods
+// A real life car has methods like start and stop:
+
+// JavaScript Variables
+// JavaScript variables are containers for data values.
+
+// How to Define a JavaScript Object
+
+// Using an Object Literal
+const litPerson = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" }
+
+// Using the new Keyword
+const newPerson = new Object();
+
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+// Using an Object Constructor
+
+// Objects are containers for Properties and Methods.
+// Properties are named Values.
+// Methods are Functions stored as Properties.
+// Properties can be primitive values, functions, or even other objects.
+
+// JavaScript Primitives
+// A primitive value is a value that has no properties or methods.
+
+// 3.14 is a primitive value
+// A primitive data type is data that has a primitive value.
+
+// JavaScript defines 7 types of primitive data types:
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+// bigint
+// Immutable
+// Primitive values are immutable (they are hardcoded and cannot be changed).
+
+// if x = 3.14, you can change the value of x, but you cannot change the value of 3.14.
+// JavaScript Objects are Mutable
+// Objects are mutable: They are addressed by reference, not by value.
